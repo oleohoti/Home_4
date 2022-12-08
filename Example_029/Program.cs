bf46ab21def801f9ec0b86e8b04110b5ff2956d9
Vladimir <vova.okhotin@mail.ru>
2022-12-08 13:52:13 +0800

    29.1

--- Example_029/Program.cs
+++ Example_029/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
-int [] array = new int[8];
-void InputArray(int[]array)
+
+void InputArray()
 {
+    int [] array = new int[8];
     for(int i=0; i<array.Length;i++)
     {
     Console.WriteLine($"Введите {i+1} элемент массива");
@@ -11,4 +12,4 @@ void InputArray(int[]array)
     for (int i = 0; i < array.Length; i++) Console.Write($" {array[i]} ");
 
 }
-InputArray(array);
+InputArray();
