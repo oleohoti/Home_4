4e440fb5cd4d31e381355fa12e2b00a052fb33dd
Vladimir <vova.okhotin@mail.ru>
2022-12-08 13:29:09 +0800

    30.1

--- Example_030/Program.cs
+++ Example_030/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Задача 30: - HARD необязательная Напишите программу, которая выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке.. Далее надо посчитать количество нулей и единиц, если единиц больше чем нулей то вывести TRUE на экран, иначе вывести False.
-int[]rendr(int len)
+int[]rendr(int len)// Метод создания массива и вывода на экран элементов
 {
     int[]array=new int[len];
     Random rnd = new Random();
-    for(int i=0; i<len;i++){array[i] = rnd.Next(0,2);}
+    for(int i=0; i<len;i++){array[i] = rnd.Next(0,2);Console.WriteLine(array[i]);}
     return array;
 }
-bool tf(int[]mas)
+void tf(int[]mas)// Метод вывода true|false
 {
     bool a = true;
     int zero = 0;
@@ -18,14 +18,11 @@ bool tf(int[]mas)
     }
     if (one>zero) a=true;
     else a=false;
-    return a;
+    Console.WriteLine(a);
 }
 
-int len =8;
-int[]mas = rendr(len);
-for (int i = 0; i < mas.Length; i++)
-{
-    Console.WriteLine(mas[i]);
-}
-bool input = tf(mas);
-Console.WriteLine(input);
+int len =8; // ввод размера массива
+int[]mas = rendr(len);// создание переменной массива для работы в методе tf
+
+tf(mas);// вызов метода tf
+
